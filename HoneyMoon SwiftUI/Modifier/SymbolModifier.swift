d5e3326524856500b1670e7d47eb0d2709e7bf53
Jinu Joy <jinuparekatil@gmail.com>
2023-12-03 18:26:44 +0530

    x mmark and heart symbol add while dragging

---
+++ HoneyMoon SwiftUI/Modifier/SymbolModifier.swift
@@ -0,0 +1,18 @@
+//
+//  SymbolModifier.swift
+//  HoneyMoon SwiftUI
+//
+//  Created by Jinu on 03/12/2023.
+//
+
+import SwiftUI
+
+struct SymbolModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SymbolModifier()
+}

84b80db96209007926e5e6d4d80803082ade66c8
Jinu Joy <jinuparekatil@gmail.com>
2023-12-03 15:16:01 +0530

    Added alert binding footer header etc..

---
+++ HoneyMoon SwiftUI/View/GuideView.swift
@@ -0,0 +1,18 @@
+//
+//  GuideView.swift
+//  HoneyMoon SwiftUI
+//
+//  Created by Jinu on 03/12/2023.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuideView()
+}

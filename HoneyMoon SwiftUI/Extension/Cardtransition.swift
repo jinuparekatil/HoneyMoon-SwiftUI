cb79b33d3641f5ca91cea22253b9b8a95a4b5d0d
Jinu Joy <jinuparekatil@gmail.com>
2023-12-03 19:03:30 +0530

    Animation while dregging

---
+++ HoneyMoon SwiftUI/Extension/Cardtransition.swift
@@ -0,0 +1,18 @@
+//
+//  Cardtransition.swift
+//  HoneyMoon SwiftUI
+//
+//  Created by Jinu on 03/12/2023.
+//
+
+import SwiftUI
+
+struct Cardtransition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Cardtransition()
+}
